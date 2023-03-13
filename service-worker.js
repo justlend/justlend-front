0daccf8a455afc6037dea9b3a1f8f1acc8fb8f81
justlendDao <java-tron@ip-172-10-2-241.us-east-2.compute.internal>
2023-03-13 10:43:52 +0000

    1.4.7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.db584cf584d0e656eff15f484afaef38.js"
+  "./precache-manifest.edd9a66c8ababb73c35a2715c9264bf5.js"
 );
 
 self.addEventListener('message', (event) => {
